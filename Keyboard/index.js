cee0cd4ab74b56b790d0f0cea63ac0ef876ee19e
Achyutha B J <yuvabj790@gamil.com>
2026-03-13 07:51:27 +0530

    Added Keyboard project

---
+++ Keyboard/index.js
@@ -0,0 +1,47 @@
+for (var i=0 ; i<document.querySelectorAll(".key").length;i++){
+    document.querySelectorAll(".key")[i].addEventListener("click",function (){
+        soundOnClick(this.innerHTML)
+    })
+}
+
+function soundOnClick(key){
+    switch(key.toLowerCase()){
+        case "a":
+        var a_note = new Audio('sounds/piano-a.mp3');
+        a_note.play();
+        break
+
+        case "b":
+        var b_note = new Audio('sounds/piano-b.mp3');
+        b_note.play();
+        break
+
+        case "c":
+        var c_note = new Audio('sounds/piano-c.wav');
+        c_note.play();
+        break
+
+        case "d":
+        var d_note = new Audio('sounds/piano-d.wav');
+        d_note.play();
+        break
+
+        case "e":
+        var e_note = new Audio('sounds/piano-e.wav');
+        e_note.play();
+        break
+
+        case "f":
+        var f_note = new Audio('sounds/piano-f.wav');
+        f_note.play();
+        break
+
+        case "g":
+        var g_note = new Audio('sounds/piano-g.wav');
+        g_note.play();
+        break
+
+        default:
+        console.log(this)
+    }
+}
