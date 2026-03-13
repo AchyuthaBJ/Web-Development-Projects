cdcb0f5001dc10d90ced43fb1fa100cba898653b
Achyutha B J <yuvabj790@gamil.com>
2026-03-13 08:28:55 +0530

    added black key functionalities

--- Keyboard/index.js
+++ Keyboard/index.js
@@ -1,10 +1,17 @@
 for (var i=0 ; i<document.querySelectorAll(".white").length;i++){
     document.querySelectorAll(".white")[i].addEventListener("click",function (){
-        soundOnClick(this.innerHTML)
+        soundOnClickWhite(this.innerHTML)
     })
 }
 
-function soundOnClick(key){
+for (var i=0 ; i<document.querySelectorAll(".black").length;i++){
+    document.querySelectorAll(".black")[i].addEventListener("click",function (){
+        soundOnClickBlack(this.innerHTML)
+    })
+}
+
+
+function soundOnClickWhite(key){
     switch(key.toLowerCase()){
         case "a":
         var a_note = new Audio('sounds/piano-a.mp3');
@@ -39,6 +46,38 @@ function soundOnClick(key){
         case "g":
         var g_note = new Audio('sounds/piano-g.wav');
         g_note.play();
+        break
+
+        default:
+        console.log(this)
+    }
+}
+
+function soundOnClickBlack(key){
+    switch(key.toLowerCase()){
+        case "c#":
+        var csharp_note = new Audio('sounds/piano-csharp.mp3');
+        csharp_note.play();
+        break
+
+        case "d#":
+        var dsharp_note = new Audio('sounds/piano-dsharp.mp3');
+        dsharp_note.play();
+        break
+
+        case "f#":
+        var fsharp_note = new Audio('sounds/piano-fsharp.mp3');
+        fsharp_note.play();
+        break
+
+        case "g#":
+        var gsharp_note = new Audio('sounds/piano-gsharp.mp3');
+        gsharp_note.play();
+        break
+
+        case "a#":
+        var asharp_note = new Audio('sounds/piano-asharp.mp3');
+        asharp_note.play();
         break
 
         default:
